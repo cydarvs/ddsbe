3fb056f0008f2f61971e45c92bf20a8d506f0f9f
Cyfred Odarve <cyfredodarve@gmail.com>
2021-05-26 18:16:32 +0800

    Load database.php to app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,6 +26,8 @@ $app = new Laravel\Lumen\Application(
   $app->withFacades();
   $app->withEloquent();
 
+  $app->configure('database');
+
 /*
 |--------------------------------------------------------------------------
 | Register Container Bindings
